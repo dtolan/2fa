e46544e8fd27d49853111deecaa09883bd0afdbd
Daniel Tolan <daniel.tolan@totalserversolutions.com>
2022-02-15 11:50:00 +0000

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# 2fa
+# python_2fa
+
+A fairly straight forward implementation of the onetimepass python class
+Adds the passing of a file or a secret to the class.
+Handles the requesting of the 2fa password
+
+## Implementation
+
+Grab the Raw python_2fa.py and place in your include/class directory
+
+### Example
+
+In example you will find a secret and below is the qrcode to scan for testing  
+![QRCode](https://github.com/[username]/[reponame]/blob/[branch]//example/qrcode/example.png?raw=true)
---
+++ python_2fa.py
@@ -0,0 +1,62 @@
+"""
+https://github.com/tadeck/onetimepass
+
+import onetimepass as otp
+my_secret = 'MFRGGZDFMZTWQ2LK'
+my_token = 123456 # should be probably from some user's input
+isValid = otp.valid_totp(token=my_token, secret=my_secret)
+"""
+from getpass import getpass
+import onetimepass as otp
+import base64
+from os import path
+
+
+class python_2fa:
+    secret = None
+
+    def __init__(self, secret=None, filename=None):
+
+        if secret is not None:
+            self.secret = secret.encode('utf-8')
+        elif filename is not None:
+            self.secret = self.__get_secret__(filename)
+        if self.secret is not None:
+            self.secret = self.__padding__(self.secret)
+            self.secret = base64.b32encode(base64.b32decode(self.secret))
+
+    def __padding__(self, data):
+        missing_padding = len(data) % 8
+        if missing_padding:
+            data += b'=' * (8 - missing_padding)
+        return data
+
+    def __get_secret__(self, filename):
+        if not path.exists(filename):
+            print("did not find file")
+            return None
+
+        try:
+            with open(filename, "r") as file:
+                line = file.readline()
+                return line.encode('utf-8')
+
+        except:
+            print("Failed to Open file")
+            return None
+
+    def response(self, tries=3):
+        count = 0
+        isValid = False
+        while count < tries and isValid is False:
+            count = count + 1
+            token = getpass("2FA Authentication: ")
+            if len(token) > 1:
+                isValid = otp.valid_totp(token=token, secret=self.secret)
+                print(isValid)
+        return isValid
+
+
+if __name__ == "__main__":
+    authenticator = python_2fa(filename='example/secret')  # secret=secret)
+    authenticator.response()
